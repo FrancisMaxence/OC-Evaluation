199b811c91fe761704302edb34e7e3a533f28208
Vincent Mardon <vincentmardon@macbook-pro-de-vincent.home>
2015-09-11 14:08:32 +0200

    Add content in catch.js

---
+++ catch.js
@@ -0,0 +1,22 @@
+$(function() {
+	// Define the window dimensions
+	var width  = ($(window).width() - 130),
+		height = ($(window).height() - 18);
+	
+	// Initial position of th <p>
+	var p = $('#target').offset();
+	p.top = 100;
+	p.left = 100;
+	$('#target').offset(p);
+	
+	// Random positioning during a hover on the <p>
+	$('#target').hover(function() {
+		var x = Math.floor(Math.random() * width),
+			y = Math.floor(Math.random() * height);
+		
+		var p = $('#target').offset();
+		p.left = x;
+		p.top = y;
+		$('#target').offset(p);
+	});
+});
